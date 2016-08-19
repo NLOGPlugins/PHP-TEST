c86cf98bb7609a41edfabb38065ffb41e8644943
ParkChanSol <sorisem4106@naver.com>
2016-08-19 17:26:54 +0900

    command

---
+++ FirstStep/명령어 받기(5강)/src/Firststep/Main.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace FirstStep;
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
+use pocketmine\utils\TextFormat;
+class Main extends PluginBase implements Listener {
+	public function onEnable() {
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+	}
+	public function onCommand(CommandSender $player, Command $command, $label, array $args) {
+		if (strtolower( $command->getName () ) == "firststep")
+			$this->getLogger()->info( TextFormat::DARK_AQUA . "명령어가 실행되었습니다");
+	}
+}
+
+?>
